827389387f7540554f7a267ab913bb681e0436e1
phamxuansang241 <79781763+phamxuansang241@users.noreply.github.com>
2021-04-26 00:26:19 +0700

    Add files via upload

---
+++ Preorder.cpp
@@ -0,0 +1,48 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct BinaryTreeNode{
+    int data;
+    BinaryTreeNode* left;
+    BinaryTreeNode* right;
+};
+
+BinaryTreeNode* createNewNode(int data){
+    BinaryTreeNode* node = new BinaryTreeNode();
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+void preOrder(struct BinaryTreeNode* root){
+     if (root == NULL) // or if (!root)
+          return;
+     printf("%d ", root->data);
+     preOrder(root->left);
+     preOrder(root->right);
+}
+
+int testPreOrder(){
+    /* Sample binary tree is
+            9
+          /   \
+        19      1
+         \    /
+          3  6
+    */
+    struct BinaryTreeNode* root = createNewNode(9);
+    root->left        = createNewNode(19);
+    root->right       = createNewNode(1);
+    struct BinaryTreeNode* temp = root->left;
+    temp->right  = createNewNode(3);
+    temp = root->right;
+    temp->left = createNewNode(6);
+    preOrder(root);
+    return 0;
+}
+
+int main() {
+    testPreOrder();
+}
